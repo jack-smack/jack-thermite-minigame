c92643a5c578a0409f7eddf202ab6a4d620844f9
Jack Maxwell <34254615+jackfmaxwell@users.noreply.github.com>
2024-10-24 13:54:53 -0400

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,38 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+author 'JACK'
+
+description 'Thermite minigame match3 style'
+
+version '0.0.1'
+
+lua54 'yes'
+
+client_scripts {
+
+}
+server_scripts {
+
+}
+shared_script {
+
+}
+
+
+ui_page 'html/index.html'
+
+files {
+	'html/index.html',
+	'html/styles/*.css',
+	'html/js/*.js',
+	'html/assets/*.png',
+	'html/assets/*.jpg',
+	'html/assets/shop/*.png',
+	'html/assets/shop/*.jpg',
+	'html/assets/*.svg',
+	'html/assets/audio/*.mp3',
+	'html/assets/audio/*.wav',
+}
+
